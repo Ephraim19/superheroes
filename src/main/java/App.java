821dccd9a3bf37e45cc1eb55444cc277b178cc34
Ephraim19 <machayoephraim@gmail.com>
2022-02-24 15:44:04 +0300

    Added java logic to get the user inputs from the template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,14 +4,36 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.modelAndView;
-
+import static spark.Spark.*;
+import models.Hero;
 public class App {
     public static void main(String[] args) {
         get("/",(request, response) -> {
             Map<String,Object> model = new HashMap<>();
             return new ModelAndView(model,"index.hbs");
         },new HandlebarsTemplateEngine());
+        post("/heroes",(request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams ("name");
+            String age = request.queryParams ("age");
+            Integer myAge = Integer.valueOf(age);
+            String power = request.queryParams ("power");
+            String weakness = request.queryParams ("weakness");
+            Hero hero = new Hero(name,myAge,power,weakness);
+            return new ModelAndView(model,"success.hbs");
+        },new HandlebarsTemplateEngine());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
