04a921447241650c5c9f5f16b2d3c88a3bda16e6
Ephraim19 <machayoephraim@gmail.com>
2022-02-24 12:44:05 +0300

    Created java classes that contain heroes object and squad object

---
+++ src/main/java/module/Hero.java
@@ -0,0 +1,15 @@
+package module;
+
+public class Hero {
+    private String name;
+    private Integer age;
+    private String specialPower;
+    private String weakness;
+
+    public Hero(String name, Integer age, String specialPower,String weakness) {
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+    }
+}
---
+++ src/main/java/module/Squad.java
@@ -0,0 +1,13 @@
+package module;
+
+public class Squad {
+    private Integer maxSize;
+    private String name;
+    private  String dedicatedCause;
+
+    public Squad(Integer maxSize,String name, String dedicatedCause) {
+        this.maxSize = maxSize;
+        this.name = name;
+        this.dedicatedCause = dedicatedCause;
+    }
+}
